46300fb775f29824c17ccb9c0805a5495a65c3fc
kashishjivani <kashishjivani2002@gmail.com>
2024-02-06 19:38:40 +0530

    added custom dropdown for filtering priority

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,8 @@ import {
   Edit,
   Toolbar,
 } from "@syncfusion/ej2-react-treegrid";
+import { createElement } from '@syncfusion/ej2-base';
+import { DropDownList } from '@syncfusion/ej2-dropdowns';
 
 let isNotLoggedIn = true;
 
@@ -33,6 +35,29 @@ export default function Home() {
   };
   const toolbarSettings = ["Add", "Delete"];
 
+  const filterSettings = { type: "Menu" };
+
+  let dropInstance;
+  const filterTypeDropdown = {
+    ui: {
+      create: (args) => {
+          const flValInput = createElement('input', { className: 'flm-input' });
+          args.target.appendChild(flValInput);
+          dropInstance = new DropDownList({
+              dataSource: ["Low", "Normal", "High", "Critical"],
+              placeholder: 'Select a value',
+          });
+          dropInstance.appendTo(flValInput);
+      },
+      read: (args) => {
+          args.fltrObj.filterByColumn(args.column.field, args.operator, dropInstance.value);
+      },
+      write: (args) => {
+          dropInstance.value = args.filteredValue;
+      }
+  }
+};
+
   return (
     <>
       <Link to="/customComponent">
@@ -47,6 +72,7 @@ export default function Home() {
         allowFiltering={true}
         editSettings={editOptions}
         toolbar={toolbarSettings}
+        filterSettings={filterSettings}
       >
         <Inject services={[Page, Sort, Filter, Edit, Toolbar]} />
         <ColumnsDirective>
@@ -79,6 +105,7 @@ export default function Home() {
             field="priority"
             headerText="Priority"
             editType="dropdownedit"
+            filter={filterTypeDropdown}
           ></ColumnDirective>
           <ColumnDirective
             field="approved"
